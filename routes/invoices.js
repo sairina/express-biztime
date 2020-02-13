f254e33dfbc8622ac4637f3b2806e8b56a465bf4
sairina <sairina.merinotsui@gmail.com>
2020-02-13 14:32:44 -0800

    added GET route for single invoice id

--- routes/invoices.js
+++ routes/invoices.js
@@ -15,6 +15,39 @@ router.get("/", async function (req, res, next) {
   }
 });
 
+router.get("/:id", async function (req, res, next) {
+  try {
+    const invoiceRes = await db.query(
+      `SELECT id, amt, paid, add_date, paid_date, comp_code
+      FROM invoices
+      WHERE id=$1`,
+      [req.params.id]
+    );
+
+    if (invoiceRes.rows.length === 0) throw new ExpressError("No such invoice!", 404);
+
+    const companyRes = await db.query(
+      `SELECT code, name, description
+      FROM companies
+      WHERE code=$1`,
+      [invoiceRes.rows[0].comp_code]
+    );
+
+    let { id, amt, paid, add_date, paid_date } = invoiceRes.rows[0];
+    let { code, name, description } = companyRes.rows[0];
+    
+    return res.json({
+      invoice: {
+        id, amt, paid, add_date, paid_date,
+        company: { code, name, description }
+      }
+    });
+  }
+  catch (err) {
+    return next(err);
+  }
+});
+
 
 
 module.exports = router;
